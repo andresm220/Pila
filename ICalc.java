153c38d7137d3c39cd41cd90edc74decd4869ae2
andresm220 <123190520+andresm220@users.noreply.github.com>
2024-01-21 11:27:10 -0600

    Definición de clases e interfaces

---
+++ ICalc.java
@@ -0,0 +1,10 @@
+public interface ICalc {
+    public void calculate();
+    public void sumar();
+    public void resta();
+    public void multiplicacion();
+    public void division();
+    public String readTXT();
+    public boolean isNumeric(String value);
+    
+}
