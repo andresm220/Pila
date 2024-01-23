e4a95a71f11123bcaf34903e9824cacd24a36228
andresm220 <123190520+andresm220@users.noreply.github.com>
2024-01-22 19:43:28 -0600

    Funciones de suma y resta

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,52 @@
-public class Calculadora{
+/**
+ * Implementación de la interfaz ICalc que realiza operaciones aritmeticas
+ * utilizando una pila (stack) de enteros.
+ */
+public class Calculadora implements ICalc {
+
+    /**
+     * Pila (stack) utilizada para almacenar operandos.
+     */
     private ICustom<Integer> stack;
+
+    /**
+     * Constructor que recibe una pila (stack) como parámetro.
+     *
+     * @param stack La pila a utilizar para almacenar operandos.
+     */
     public Calculadora(ICustom<Integer> stack) {
         this.stack = stack;
+    }
+
+    /**
+     * Realiza la operación de suma tomando dos operandos de la pila.
+     *
+     * @return El resultado de la suma.
+     * @throws IllegalArgumentException Si faltan operandos para la suma.
+     */
+    public int sumar() {
+        Integer operandoB = stack.pop();
+        Integer operandoA = stack.pop();
+        if (operandoA != null && operandoB != null) {
+            return operandoA + operandoB;
+        } else {
+            throw new IllegalArgumentException("Faltan operandos para la suma.");
+        }
+    }
+
+    /**
+     * Realiza la operación de resta tomando dos operandos de la pila.
+     *
+     * @return El resultado de la resta.
+     * @throws IllegalArgumentException Si faltan operandos para la resta.
+     */
+    public int resta() {
+        Integer operandoB = stack.pop();
+        Integer operandoA = stack.pop();
+        if (operandoA != null && operandoB != null) {
+            return operandoA - operandoB;
+        } else {
+            throw new IllegalArgumentException("Faltan operandos para la resta.");
+        }
     }
 }
